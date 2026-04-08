01dac4b2936861b4c4e9f980faf6234cac785ec0
TizianoCausin01 <causintiziano@gmail.com>
2026-04-08 19:38:43 +0200

    Developed functions to do the subsampling in a systematic way for II, dRSA and regression

--- python_scripts/src/project_specific_utils/__init__.py
+++ python_scripts/src/project_specific_utils/__init__.py
@@ -1,5 +1,5 @@
-__all__ = ['load_eyetracking_data', 'load_meg_data', 'get_spaced_pseudotrials',]
+__all__ = ['load_eyetracking_data', 'load_meg_data', 'get_spaced_pseudotrials', 'subsampling_lagged_comparisons', 'subsampling_RSA', 'subsampling_II', 'subsampling_encoding']
 
 from .dataloader import load_eyetracking_data, load_meg_data
-from .utils import run2part, get_spaced_pseudotrials
+from .utils import run2part, get_spaced_pseudotrials, subsampling_lagged_comparisons, subsampling_RSA, subsampling_II, subsampling_encoding
 
--- python_scripts/src/project_specific_utils/utils.py
+++ python_scripts/src/project_specific_utils/utils.py
@@ -1,10 +1,18 @@
-import sys, os
+import sys, os, yaml
 import warnings
-import h5py
+import numpy as np
 import random
+ENV = os.getenv("MY_ENV", "dev")
+with open("../../config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+paths = config[ENV]["paths"]
+sys.path.append(paths["useful_stuff_path"])
 sys.path.append("..")
+from useful_stuff.general_utils.RSA import dRSA
+from useful_stuff.general_utils.II import dynInformationImbalance
+from useful_stuff.general_utils.regression import dyn_linear_encoding
+from useful_stuff.general_utils.utils import print_wise, TimeSeries, compatible_TimeSeries_check
 from project_specific_utils.dataloader import load_eyetracking_data
-from general_utils.utils import print_wise, TimeSeries
 
 '''
 run2part
@@ -70,3 +78,124 @@ def get_spaced_pseudotrials(signal: TimeSeries, pseudotrials_len_tps: int, pseud
         raise ValueError("Not enough non-overlapping pseudotrials available for the given max_lag.")
     return pseudotrials_idx
 # EOF
+
+'''
+subsampling_lagged_comparisons
+Performs subsampled lagged comparisons between signal and model across multiple pseudotrials and iterations, averaging the resulting similarity matrices.
+
+INPUT:
+    - signal: TimeSeries
+    - model: TimeSeries
+    - pseudotrials_len_tps: int -> length of each pseudotrial in timepoints
+    - iteration_n: int -> number of subsampling iterations
+    - pseudotrials_n: int -> number of pseudotrials per iteration
+    - discontinuities: list[int] -> timepoints to avoid when sampling pseudotrials
+    - func: function -> function computing similarity (e.g. RSA, II)
+    - rank: int -> rank for distributed logging
+    - *args: additional positional arguments for func
+    - min_separation: int -> minimum separation between pseudotrials
+    - **kwargs: additional keyword arguments for func
+
+OUTPUT:
+    - tot_similarity_A2B: ndarray -> averaged similarity matrix (A2B)
+    - tot_similarity_B2A: ndarray -> averaged similarity matrix (B2A)
+'''
+def subsampling_lagged_comparisons(signal, model, pseudotrials_len_tps, iteration_n, pseudotrials_n, discontinuities, func, rank, *args, min_separation=5, **kwargs):
+    compatible_TimeSeries_check(signal, model)    
+    tot_similarity_A2B = np.zeros((pseudotrials_len_tps, pseudotrials_len_tps))
+    tot_similarity_B2A = np.zeros((pseudotrials_len_tps, pseudotrials_len_tps))
+    # end if measure=='RSA':
+    for i_iter in range(iteration_n):
+        if i_iter %10 ==0:
+            print_wise(f"starting iteration {i_iter} of {iteration_n-1}", rank=rank)
+        signal_trials = []
+        model_trials = []
+        pseudotrials_idx = get_spaced_pseudotrials(signal, pseudotrials_len_tps, pseudotrials_n, min_separation, discontinuities=discontinuities)
+        for n in pseudotrials_idx:
+            signal_trials.append(signal.get_array()[:,n:n+pseudotrials_len_tps])
+            model_trials.append(model.get_array()[:,n:n+pseudotrials_len_tps])
+        # end for n in pseudotrials_idx:
+        signal_trials = TimeSeries(np.stack(signal_trials, axis=2), signal.get_fs())
+        model_trials = TimeSeries(np.stack(model_trials, axis=2), model.get_fs())
+        # if func is subsampling_RSA the second output is just a 0 for compatibility with the double output of II
+        similarity_corr_A2B, similarity_corr_B2A = func(signal_trials, model_trials, *args, **kwargs) 
+        tot_similarity_A2B += similarity_corr_A2B
+        tot_similarity_B2A += similarity_corr_B2A
+    # end for i_iter in range(iteration_n):
+    tot_similarity_A2B = tot_similarity_A2B/iteration_n
+    tot_similarity_B2A = tot_similarity_B2A/iteration_n
+    return tot_similarity_A2B, tot_similarity_B2A
+# EOF
+
+'''
+subsampling_II
+Computes dynamic information imbalance (II) between neural and model pseudotrials.
+
+INPUT:
+    - neu_trials: TimeSeries -> neural data pseudotrials
+    - mod_trials: TimeSeries -> model data pseudotrials
+    - neu_metric: callable -> metric for neural data
+    - mod_metric: callable -> metric for model data
+    - k: int -> number of neighbors for II computation
+
+OUTPUT:
+    - II_A2B: ndarray -> information imbalance from neural to model
+    - II_B2A: ndarray -> information imbalance from model to neural
+'''
+def subsampling_II(neu_trials, mod_trials, neu_metric, mod_metric, k=1):
+    II_obj = dynInformationImbalance(neu_metric, mod_metric, k=k)
+    II_obj.compute_both_RDM_timeseries(neu_trials, mod_trials)
+    II_obj.compute_both_distance_ranks_timeseries()
+    II_A2B, II_B2A = II_obj.compute_both_dynII()
+    return II_A2B, II_B2A
+# EOF
+
+
+'''
+subsampling_RSA
+Computes representational similarity analysis (RSA) between neural and model pseudotrials.
+
+INPUT:
+    - neu_trials: TimeSeries -> neural data pseudotrials
+    - mod_trials: TimeSeries -> model data pseudotrials
+    - neu_metric: callable -> metric for neural data
+    - mod_metric: callable -> metric for model data
+    - RSA_metric: str -> similarity metric used for RSA
+
+OUTPUT:
+    - rsa_corr: ndarray -> RSA similarity matrix (A2B)
+    - _: int -> placeholder (compatibility with II output format)
+'''
+def subsampling_RSA(neu_trials, mod_trials, neu_metric, mod_metric, RSA_metric="correlation"):
+    drsa_obj_corr = dRSA(neu_metric, model_RDM_metric=mod_metric, RSA_metric=RSA_metric)
+    drsa_obj_corr.compute_both_RDM_timeseries(neu_trials, mod_trials)
+    rsa_corr = drsa_obj_corr.compute_dRSA()
+    _ = 0 # just for compatibility with II
+    return rsa_corr, _
+# EOF
+
+
+'''
+subsampling_encoding
+Computes dynamic encoding performance between model and neural pseudotrials using cross-validated regression.
+
+INPUT:
+    - neu_trials: TimeSeries -> neural data pseudotrials (targets)
+    - mod_trials: TimeSeries -> model data pseudotrials (features)
+    - regression_type: str -> type of regression model
+    - cv_type: str -> cross-validation strategy
+    - max_lag: int -> maximum temporal lag considered
+    - score_type: str -> scoring metric (default 'r2')
+    - n_splits: int -> number of CV splits
+
+OUTPUT:
+    - final_score: ndarray -> encoding performance scores
+'''
+def subsampling_encoding(neu_trials, mod_trials, regression_type, cv_type, max_lag, score_type='r2', n_splits=5):
+    regression_obj = dyn_linear_encoding(regression_type, cv_type, max_lag, score_type=score_type, n_splits=n_splits)
+    final_score = regression_obj.crossvalidate_general_dyn(mod_trials, neu_trials)
+    print(final_score.shape())
+    return final_score.get_array()
+# EOF 
+
+
