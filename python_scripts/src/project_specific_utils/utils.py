a0bfad717bfadb5755dc6ce2683a83a3107a43bf
TizianoCausin01 <causintiziano@gmail.com>
2026-04-08 19:11:20 +0200

    Added the discontinuities to take out from the pseudotrials

--- python_scripts/src/project_specific_utils/utils.py
+++ python_scripts/src/project_specific_utils/utils.py
@@ -1,4 +1,5 @@
 import sys, os
+import warnings
 import h5py
 import random
 sys.path.append("..")
@@ -32,19 +33,33 @@ INPUT:
     - pseudotrials_len_tps: int -> length of each pseudotrial in timepoints
     - pseudotrials_n: int -> number of pseudotrials to sample
     - min_separation: int -> minimum required separation (in timepoints) between pseudotrials (in addition to their length)
-
+    - discontinuities: list -> the points we don't want to cross with our pseudotrials (e.g. because it's crossing some run boundary)
+    - fail_safety: bool -> whether or not having a deterministic failure if there is very little room for wiggle the pseudotrials
 OUTPUT:
     - pseudotrials_idx: list of ints -> starting indices of non-overlapping pseudotrials
 
 RAISES:
     - ValueError: if the signal is too short to fit the required number of spaced pseudotrials
 """
-def get_spaced_pseudotrials(signal, pseudotrials_len_tps, pseudotrials_n, min_separation):
+def get_spaced_pseudotrials(signal: TimeSeries, pseudotrials_len_tps: int, pseudotrials_n: int, min_separation: int, discontinuities: list=[], fail_safety: bool=True):
     candidates = list(range(len(signal) - pseudotrials_len_tps)) # all the possible points that can be chosen as pseudotrial start [0, 1, 2, ..., n-1]
     pseudotrials_idx = []
     tot_separation = pseudotrials_len_tps+min_separation # the minimum separation between two pseudotrials starts (so that they don't overlap)
-    if tot_separation*pseudotrials_n > len(signal):
+    if discontinuities:
+        to_remove = []
+        for el in discontinuities:
+            to_remove.extend(list(range(max(el-pseudotrials_len_tps,0), el)))
+        to_remove = set(to_remove) # for O(1) lookup
+        candidates = [x for x in candidates if x not in to_remove]
+    # tot_separation is multiplied by the amount of pseudotrials_n and then we check if the projected pseudotrials duration surpasses the whole signal duration
+    if tot_separation*pseudotrials_n - 2*min_separation > len(signal) - len(discontinuities):  #-2*min_separation bc the first and last trials don't have it, -len(discontinuities)*pseudotrials_len_tps not to count the pts we don't want to cross 
         raise ValueError(f"The length of the TimeSeries {len(signal)} is not enough for the number and length of pseudotrials required")
+    rho = len(candidates)/(pseudotrials_n*tot_separation) # rho=1 means that you have barely enough space, rho=1.2 means that you have just 20% of room, etc...
+    if rho < 1.2:
+        if fail_safety:
+            raise IndexError(f"For {pseudotrials_n=} and pseudotrial_len={pseudotrials_len_tps} it's likely you'll encounter a failure")
+        else:
+            warnings.warn(f"For {pseudotrials_n=} and pseudotrial_len={pseudotrials_len_tps} it's likely you'll encounter a failure")
     # while still there are candidates available and while there are and while we haven't reached the quota of pseudotrials
     while candidates and len(pseudotrials_idx) < pseudotrials_n: 
         idx = random.choice(candidates) # randomly chooses among the available timepoints
