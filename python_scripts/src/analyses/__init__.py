129af54e207fa2fa24d75b3dbbf3828adad246c5
TizianoCausin01 <causintiziano@gmail.com>
2026-04-14 10:12:32 +0200

    More durable solution for circular import

---
+++ python_scripts/src/analyses/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['get_spaced_pseudotrials', 'subsampling_lagged_comparisons', 'subsampling_RSA', 'subsampling_II', 'subsampling_encoding', 'save_lagged_comparisons', 'multivariate_lagged_comparisons',]
+
+from .subsampling_lagged_comparisons import get_spaced_pseudotrials, subsampling_lagged_comparisons, subsampling_RSA, subsampling_II, subsampling_encoding, save_lagged_comparisons, multivariate_lagged_comparisons
+
+
