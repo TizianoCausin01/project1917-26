dbad7d87542b909e87e7ba1641f8abf1bbe57117
TizianoCausin01 <causintiziano@gmail.com>
2026-04-14 01:56:06 -0400

    Trying to fix a circular import

--- python_scripts/src/project_specific_utils/__init__.py
+++ python_scripts/src/project_specific_utils/__init__.py
@@ -2,6 +2,6 @@ __all__ = ['load_eyetracking_data', 'load_meg_data', 'get_spaced_pseudotrials', 
 
 from .dataloader import load_eyetracking_data, load_meg_data, load_concat_regressout_meg, load_concat_regressout_mod, load_concat_gaze
 from .utils import run2part
-from .subsampling_lagged_comparisons import get_spaced_pseudotrials, subsampling_lagged_comparisons, subsampling_RSA, subsampling_II, subsampling_encoding, save_lagged_comparisons, multivariate_lagged_comparisons
+#from .subsampling_lagged_comparisons import get_spaced_pseudotrials, subsampling_lagged_comparisons, subsampling_RSA, subsampling_II, subsampling_encoding, save_lagged_comparisons, multivariate_lagged_comparisons
 
 
