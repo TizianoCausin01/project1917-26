8d4d5854266a0f47896bc06a46e230bbc96f42e1
TizianoCausin01 <causintiziano@gmail.com>
2026-04-27 13:34:14 +0200

    Improved parallelization of lagged linear encoding

--- python_scripts/scripts/run_lagged_encoding_comparisons.py
+++ python_scripts/scripts/run_lagged_encoding_comparisons.py
@@ -21,9 +21,7 @@ from useful_stuff.general_utils.utils import TimeSeries
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--analysis_type", type=str)
-parser.add_argument("--sub_num", type=int)
 parser.add_argument("--sensors_group", type=str)
-parser.add_argument("--repetition", type=int)
 parser.add_argument("--max_lag", type=int)
 parser.add_argument("--regression_type", type=str)
 parser.add_argument("--score_type", type=str)
@@ -42,17 +40,17 @@ parser.add_argument("--pooling", type=str, default='all')
 
 cfg = parser.parse_args()
 layer_names = get_relevant_output_layers(cfg.model_name, pkg=cfg.pkg) 
-task_list = [f"{cfg.model_name}_{l}" for l in layer_names]
 mod_fs = config["movie_fs"]
+subjects = config["subjects"][:10]
+layers = [f"{cfg.model_name}_{l}" for l in layer_names]
+repetitions = [0, 1]
+task_list = [
+    (subject, layer, rep)
+    for subject in subjects
+    for layer in layers
+    for rep in repetitions
+]
 model_len = [round(i*cfg.neu_fs/config["movie_fs"]) for i in config["model_len"]]
 cfg.timepts_to_regress_out = (-cfg.timepts_to_regress_out, cfg.timepts_to_regress_out)
-_, rank, _ = parallel_setup()
-if rank != 0:
-    n = load_concat_regressout_meg(paths, cfg.sub_num, cfg.repetition, cfg.sensors_group, cfg.neu_fs, cfg.gaze_fs, cfg.regress_out_gaze, cfg.PCs_to_regress_out, timepts_to_regress_out=cfg.timepts_to_regress_out, rank=rank)
-    n.z_score_feats()
-    n = TimeSeries(n.get_array()[:,:,np.newaxis], cfg.neu_fs)
-else:
-    n = None
-# end if rank != 0:
 
-master_workers_queue(task_list, paths, lagged_encoding_comparisons, *(n, cfg.analysis_type, cfg.sub_num, cfg.sensors_group, cfg.repetition, cfg.max_lag, cfg.neu_fs, mod_fs, cfg.regression_type, cfg.score_type, cfg.sq_side, cfg.regress_out_gaze, cfg.n_model_components, cfg.model_PCs_to_keep,), **{"pooling": cfg.pooling}) 
+master_workers_queue(task_list, paths, lagged_encoding_comparisons, *(cfg.analysis_type, cfg.sensors_group, cfg.max_lag, cfg.neu_fs, mod_fs, cfg.regression_type, cfg.score_type, cfg.sq_side, cfg.regress_out_gaze, cfg.PCs_to_regress_out, cfg.timepts_to_regress_out, cfg.n_model_components, cfg.model_PCs_to_keep,), **{"pooling": cfg.pooling}) 
